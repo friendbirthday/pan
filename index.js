aafd0bf0035f9aa87eb01628a4fe2d814743f381
friendbirthday <dr3253410443@163.com>
2025-04-05 20:13:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||罗|艺|同学|你好！|祝|你|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3||小|胖|同学|你好！|祝|你|生|日|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
